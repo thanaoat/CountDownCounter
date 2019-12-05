df7127e40147b5a394d684bc22073e840eb27f99
Thananon Kumkrong <thananon.oat@gmail.com>
2019-12-05 15:19:11 +0700

    Add count text and count down button

--- app/src/main/java/thanaoat/android/countdowncounter/MainActivity.kt
+++ app/src/main/java/thanaoat/android/countdowncounter/MainActivity.kt
@@ -2,6 +2,7 @@ package thanaoat.android.countdowncounter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 
@@ -14,5 +15,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvCount = findViewById(R.id.tvCount)
+        btnCountDown = findViewById(R.id.btnCountDown)
+
+        tvCount.text = count.toString()
+        btnCountDown.setOnClickListener {
+            count -= 1
+
+            // TODO: Play sound
+            updateTvCount()
+        }
+
+    }
+
+    private fun updateTvCount() {
+        tvCount.setText(count.toString())
     }
 }
